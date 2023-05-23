1cff65113810d52ef6d28597d320916e4a9747a7
Adam Markie <adam.markie@gmail.com>
2023-05-23 09:23:51 +0100

    Displaying map marker

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-'use strict';
+'use strict'
 
 // prettier-ignore
-const months = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
+const months = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ]
 
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+const form = document.querySelector('.form')
+const containerWorkouts = document.querySelector('.workouts')
+const inputType = document.querySelector('.form__input--type')
+const inputDistance = document.querySelector('.form__input--distance')
+const inputDuration = document.querySelector('.form__input--duration')
+const inputCadence = document.querySelector('.form__input--cadence')
+const inputElevation = document.querySelector('.form__input--elevation')
 
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(function (position) {
@@ -19,16 +19,28 @@ if (navigator.geolocation) {
 
     const coords = [ latitude, longitude ]
 
-    const map = L.map('map').setView(coords, 13);
+    const map = L.map('map').setView(coords, 13)
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    }).addTo(map)
 
-    L.marker(coords).addTo(map)
-      .bindPopup('You here bitch')
-      .openPopup();
-    },
+    map.on('click', function (mapEvent) {
+      console.log(mapEvent)
+      const { lat, lng } = mapEvent.latlng
+
+      L.marker({ lat, lng }).addTo(map)
+        .bindPopup(L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup'
+        }))
+        .openPopup()
+
+    })
+  },
     function () {
       alert('Could not get location')
     })
